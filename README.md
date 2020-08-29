c21fa142b05bba489361fa2bee62c992d12e9c93
Valentino Erlangga Setligt Gaghana <gaghanav@gmail.com>
2020-08-29 23:42:07 +0700

    revision readme.md for confusion on database creation and database import

--- README.md
+++ README.md
@@ -2,20 +2,25 @@
 
 ## Company CRUD with AngularJs for the FrontEnd and Java SpringBoot for the BackEnd
 
-Requirements
+## 1. Requirements
 ```text
 - Java version 1.8.0
 - AngularJs version 1.7.5
 ```
-## DB Settings
+## 2. DB Settings
 ```text
 This is the DataBase settings used when creating this project
-DB name : company
 DB username : root
 DB password :
 ```
+## 3. Create Empty DataBase
+```text
+- Open terminal
+- type "mysql -u {DB_USERNAME} -p"
+- then type "CREATE DATABASE company;"
+```
 
-## BackEnd application.properties settings
+## 3. BackEnd application.properties settings
 ```code
 spring.datasource.url=jdbc:mysql://localhost/company?useSSL=false&serverTimezone=UTC
 spring.datasource.username={YOUR_DB_USERNAME}
@@ -27,26 +32,29 @@ spring.jpa.hibernate.ddl-auto=update
 spring.jpa.show-sql=true
 ```
 
-## How to import DB
-```text
-- Open terminal
-- Type "mysql -u {YOUR_DB_USERNAME} -p company < "company.sql"
-```
 
-
-## How to run AngularJs FrontEnd
-```text
-- Open terminal inside /FrontEnd folder
-- Type "npm run start"
-```
-
-## How to run Java Spring Boot BackEnd
+## 4. How to run Java Spring Boot BackEnd
 ```text
 #NOTE:You Must Use DB Username "Root" and DB Password "" or Blank to do below steps 
 
 - Open terminal inside /BackEnd/target folder
 - Type "java -jar valentino-0.1.jar"
 ```
+
+## 5. How to Auto Populate Employee & Position Table
+```text
+- Open terminal
+- Type "mysql -u {YOUR_DB_USERNAME} -p company < "company.sql"
+```
+
+## 6. How to run AngularJs FrontEnd
+```text
+- Open terminal inside /FrontEnd folder
+- Type "npm run start"
+```
+
+
+## 7. ScreenShots
 ## Front End Index
 ![image](screenshot/FE-index.PNG)
 
--- company.sql
+++ company.sql
@@ -7,36 +7,6 @@
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `company`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employee`
---
-
-CREATE TABLE `employee` (
-  `id` int(11) NOT NULL,
-  `birth_date` date DEFAULT NULL,
-  `gender` int(11) DEFAULT NULL,
-  `id_number` int(11) DEFAULT NULL,
-  `is_delete` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `position_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 --
 -- Dumping data for table `employee`
 --
@@ -47,19 +17,6 @@ INSERT INTO `employee` (`id`, `birth_date`, `gender`, `id_number`, `is_delete`, 
 (3, '1993-04-20', 2, 20041993, 0, 'Rosiana', 4),
 (4, '1994-01-11', 1, 11011994, 0, 'Yudi Ismiaji', 3),
 (5, '1995-05-22', 1, 99991111, 1, 'Valen', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `position`
---
-
-CREATE TABLE `position` (
-  `id` int(11) NOT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `is_delete` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `position`
@@ -72,44 +29,3 @@ INSERT INTO `position` (`id`, `code`, `is_delete`, `name`) VALUES
 (4, 'TEST', 0, 'Tester'),
 (5, 'HELP', 0, 'Helpdesk');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKbc8rdko9o9n1ri9bpdyxv3x7i` (`position_id`);
-
---
--- Indexes for table `position`
---
-ALTER TABLE `position`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `employee`
---
-ALTER TABLE `employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `employee`
---
-ALTER TABLE `employee`
-  ADD CONSTRAINT `FKbc8rdko9o9n1ri9bpdyxv3x7i` FOREIGN KEY (`position_id`) REFERENCES `position` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
